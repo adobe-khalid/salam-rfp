39e75f41b4791c75eec5f3e44f61c1d607c47e00
Md Khalid <kahlid@adobe.com>
2024-01-11 15:12:32 +0530

    Salam RFP POC - Latest code

--- src/App.js
+++ src/App.js
@@ -3,225 +3,232 @@ import Box from "@mui/material/Box";
 import Slider from "@mui/material/Slider";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
+import AppBar from "@mui/material/AppBar";
+import React, { useEffect, useState, useRef } from "react";
+import Stack from "@mui/material/Stack";
+import Radio from "@mui/material/Radio";
+import RadioGroup from "@mui/material/RadioGroup";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import FormControl from "@mui/material/FormControl";
+import FormLabel from "@mui/material/FormLabel";
 
+import {
+  validity,
+  smsLimit,
+  dataLimit,
+  socialMediaLimit,
+} from "./sliderValues";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+
+function validityText(value) {
+  return `${value} days`;
+}
+
+function smsLimitText(value) {
+  return `${value} sms`;
+}
+
+function dataLimitText(value) {
+  return `${value} GB`;
+}
+
+const CustomSlider = ({
+  label,
+  ariaValueText,
+  defaultVal,
+  step = null,
+  min,
+  max,
+  marks,
+}) => {
+  return (
+    <Box sx={{ width: "100%", m: "10px" }}>
+      <Card sx={{ minWidth: "100%" }}>
+        <CardContent>
+          <Typography sx={{ fontSize: 14, fontWeight: "bold" }} align="left">
+            {label}
+          </Typography>
+          <Slider
+            sx={{ p: "10px", width: "85%" }}
+            aria-label="Custom marks"
+            defaultValue={defaultVal}
+            getAriaValueText={ariaValueText}
+            step={step}
+            min={min}
+            max={max}
+            valueLabelDisplay="off"
+            marks={marks}
+          />
+        </CardContent>
+      </Card>
+    </Box>
+  );
+};
 export default function App() {
-  const jsonData = {
-    data: {
-      data: {
-        item: {
-          _path: "/content/dam/salam/sa/assemblers/genericassembly",
-          title: null,
-          sectionHeading: "Build Your Own Package",
-          recommendedSectionHeading: "Recommendation",
-          logo: null,
-          links: [
-            { label: "Add this plan", url: null },
-            { label: "Buy Addon", url: null },
-          ],
-          addOns: [
-            { name: "500MB", validity: "7", price: "7", currencySymbol: "﷼" },
-            { name: "1GB", validity: "7", price: "12", currencySymbol: "﷼" },
-            { name: "3GB", validity: "7", price: "25", currencySymbol: "﷼" },
-            { name: "5GB", validity: "15", price: "39", currencySymbol: "﷼" },
-            { name: "10GB", validity: "7", price: "30", currencySymbol: "﷼" },
-            { name: "10GB", validity: "30", price: "65", currencySymbol: "﷼" },
-            { name: "15GB", validity: "14", price: "45", currencySymbol: "﷼" },
-            { name: "20GB", validity: "14", price: "55", currencySymbol: "﷼" },
-            { name: "20GB", validity: "30", price: "85", currencySymbol: "﷼" },
-            { name: "40GB", validity: "30", price: "85", currencySymbol: "﷼" },
-            {
-              name: "100GB",
-              validity: "30",
-              price: "160",
-              currencySymbol: "﷼",
-            },
-            {
-              name: "Unlimited",
-              validity: "30",
-              price: "325",
-              currencySymbol: "﷼",
-            },
-          ],
-          plans: [
-            {
-              name: "Solo 74",
-              type: "Voice & Data",
-              billingType: "Prepaid",
-              price: 85.68,
-              currencySymbol: "﷼",
-              validity: 30.0,
-              data: "20 GB",
-              socialMedia: "20 GB",
-              sms: "450",
-            },
-            {
-              name: "Solo 99",
-              type: "Voice & Data",
-              billingType: "Prepaid",
-              price: 113.85,
-              currencySymbol: "﷼",
-              validity: 30.0,
-              data: "35 GB",
-              socialMedia: "Unlimited",
-              sms: "450",
-            },
-            {
-              name: "Solo 149",
-              type: "Voice & Data",
-              billingType: "Prepaid",
-              price: 171.35,
-              currencySymbol: "﷼",
-              validity: 30.0,
-              data: "59 GB",
-              socialMedia: "Unlimited",
-              sms: "Unlimited",
-            },
-            {
-              name: "Solo 160",
-              type: "Voice & Data",
-              billingType: "Prepaid",
-              price: 184.0,
-              currencySymbol: "﷼",
-              validity: 60.0,
-              data: "50 GB",
-              socialMedia: "Unlimited",
-              sms: "450",
-            },
-            {
-              name: "Solo 240",
-              type: "Voice & Data",
-              billingType: "Prepaid",
-              price: 276.0,
-              currencySymbol: "﷼",
-              validity: 90.0,
-              data: "90 GB",
-              socialMedia: "Unlimited",
-              sms: "450",
-            },
-            {
-              name: "Solo 340",
-              type: "Voice & Data",
-              billingType: "Prepaid",
-              price: 333391.0,
-              currencySymbol: "﷼",
-              validity: 120.0,
-              data: "135 GB",
-              socialMedia: "Unlimited",
-              sms: "450",
-            },
-            {
-              name: "Solo 74",
-              type: "Voice & Data",
-              billingType: "Postpaid",
-              price: 85.68,
-              currencySymbol: "﷼",
-              validity: 30.0,
-              data: "20 GB",
-              socialMedia: "20 GB",
-              sms: "450",
-            },
-            {
-              name: "Solo 99",
-              type: "Voice & Data",
-              billingType: "Prepaid",
-              price: 113.85,
-              currencySymbol: "﷼",
-              validity: 30.0,
-              data: "35 GB",
-              socialMedia: "Unlimited",
-              sms: "450",
-            },
-            {
-              name: "Solo 149",
-              type: "Voice & Data",
-              billingType: "Postpaid",
-              price: 171.35,
-              currencySymbol: "﷼",
-              validity: 30.0,
-              data: "59 GB",
-              socialMedia: "Unlimited",
-              sms: "Unlimited",
-            },
-            {
-              name: "Solo Infinite",
-              type: "Voice & Data",
-              billingType: "Postpaid",
-              price: 412.85,
-              currencySymbol: "﷼",
-              validity: 30.0,
-              data: "Unlimited",
-              socialMedia: "Unlimited",
-              sms: "Unlimited",
-            },
-          ],
-          config: null,
-        },
-      },
-    },
+  const [isCalculated, setIsCalculated] = useState(false);
+  const [value, setValue] = React.useState("20 GB");
+  const [isPrepaid, setIsPrepaid] = React.useState(true);
+
+  const ref = useRef(null);
+
+  const handleCalculate = () => {
+    setIsCalculated(true);
+    ref.current?.scrollIntoView({ behavior: "smooth" });
   };
 
-  let plans = jsonData.data.data.item.plans;
-
-  let getUniqueValues = (filteredData, trailString) => {
-    let uniqueData = filteredData.filter(
-      (data, index) => filteredData.indexOf(data) === index
-    );
-    let valueLabelPair = [];
-    uniqueData.forEach((element) => {
-      let numValue = element;
-      console.log(`${element} "type" ${typeof element}`);
-      valueLabelPair.push({
-        value: numValue,
-        label: `${numValue + " " + trailString}`,
-      });
-    });
-    return valueLabelPair;
+  const handleChange = (event) => {
+    setValue(event.target.value);
   };
-
-  let validityArray = getUniqueValues(
-    plans.map((plan) => plan.validity.toString()),
-    "days"
-  );
-
-  let dataLimitArray = getUniqueValues(
-    plans.map((plan) => plan.data.toString()),
-    ""
-  );
-
-  console.log('dataLimitArray', dataLimitArray);
-
-  function valuetext(value) {
-    return `${value} days`;
-  }
-
-  function valueLabelFormat(value) {
-    return validityArray.findIndex((validity) => validity.value === value) + 1;
-  }
-
-  function defaultValue(arr) {
-    return arr[0].value;
-  }
 
   return (
     <div className="App">
-      <Box sx={{ width: 500, m: "30px" }}>
-        <Card sx={{ minWidth: 500 }}>
-          <CardContent>
-            <Slider
-              sx={{ m: "10px", width: 400 }}
-              aria-label="Restricted values"
-              defaultValue={defaultValue(validityArray)}
-              valueLabelFormat={valueLabelFormat}
-              getAriaValueText={valuetext}
-              step={null}
-              min={30}
-              max={120}
-              valueLabelDisplay="off"
-              marks={validityArray}
-            />
-          </CardContent>
-        </Card>
-      </Box>
+      <AppBar position="static" sx={{ backgroundColor: "#002035" }}>
+         {" "}
+        <img
+          src="https://salammobile.sa/wp-content/uploads/2022/03/mySalamLogo-min.png.webp"
+          width="70"
+          alt="Header"
+        />
+      </AppBar>
+      <div className="container">
+        <div>
+          <Typography
+            sx={{ fontSize: 14, fontWeight: "bold", m: "30px" }}
+            align="left"
+          >
+            Choose a billing type
+          </Typography>
+          <Box
+            display="flex"
+            justifyContent="space-between"
+            sx={{ width: 350, m: "30px" }}
+          >
+            <Button
+              sx={{ borderRadius: 28, width: 150 }}
+              variant="contained"
+              size="large"
+              className={isPrepaid ? "selected" : "unselected"}
+              onClick={() => {
+                setIsPrepaid(!isPrepaid);
+              }}
+            >
+              Prepaid
+            </Button>
+            <Button
+              sx={{ borderRadius: 28, width: 150 }}
+              variant="contained"
+              size="large"
+              className={!isPrepaid ? "selected" : "unselected"}
+              onClick={() => {
+                setIsPrepaid(!isPrepaid);
+              }}
+            >
+              Postpaid
+            </Button>
+          </Box>
+        </div>
+        <CustomSlider
+          label="Data (Regular)"
+          ariaValueText={dataLimitText}
+          step={10}
+          min={0}
+          max={300}
+          defaultVal={0}
+          marks={dataLimit}
+        />
+        <CustomSlider
+          label="Data (Social Media)"
+          ariaValueText={dataLimitText}
+          min={20}
+          max={40}
+          defaultVal={20}
+          marks={socialMediaLimit}
+        />
+        <CustomSlider
+          label="SMS Limit"
+          ariaValueText={smsLimitText}
+          min={450}
+          max={900}
+          defaultVal={450}
+          marks={smsLimit}
+        />
+        <CustomSlider
+          label="Validity"
+          ariaValueText={validityText}
+          min={30}
+          max={120}
+          defaultVal={30}
+          marks={validity}
+        />
+        <div className="calc">
+          <Box
+            display="flex"
+            justifyContent="center"
+            sx={{ width: "100%", p: "10px", m: "20px 0px" }}
+          >
+            <Button
+              sx={{ borderRadius: 28, width: 150 }}
+              variant="contained"
+              size="large"
+              onClick={handleCalculate}
+            >
+              Calculate
+            </Button>
+          </Box>
+        </div>
+
+        {isCalculated ? (
+          <div ref={ref}>
+            <Box sx={{ width: "100%" }}>
+              <Card sx={{ width: "100%", height: 150, my: "20px" }}>
+                <CardContent>
+                  <Typography sx={{ fontSize: 14 }} align="right">
+                    <Stack
+                      direction="row"
+                      justifyContent="space-between"
+                      alignItems="center"
+                    >
+                      <Typography gutterBottom component="div">
+                        Recommanded Plan
+                      </Typography>
+                      <Typography gutterBottom component="div">
+                        Solo 99
+                      </Typography>
+                    </Stack>
+                    <Stack
+                      direction="row"
+                      justifyContent="space-between"
+                      alignItems="center"
+                    >
+                      <Typography gutterBottom component="div">
+                        Add On
+                      </Typography>
+                      <Typography gutterBottom component="div">
+                        15 GB
+                      </Typography>
+                    </Stack>
+                    <Stack
+                      direction="row"
+                      justifyContent="space-between"
+                      alignItems="center"
+                    >
+                      <Typography gutterBottom variant="h6" component="div">
+                        Total Price
+                      </Typography>
+                      <Typography gutterBottom component="div">
+                        158.85 SAR
+                      </Typography>
+                    </Stack>
+                  </Typography>
+                </CardContent>
+              </Card>
+            </Box>
+          </div>
+        ) : (
+          <></>
+        )}
+      </div>
     </div>
   );
 }
