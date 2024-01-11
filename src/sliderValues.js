275fc14a5922676da30531a23aca9f5535eeb1ef
Md Khalid <kahlid@adobe.com>
2024-01-11 15:00:33 +0530

    Salam RFP - Latest Code of POC

---
+++ src/sliderValues.js
@@ -0,0 +1,71 @@
+const validity = [
+  {
+    value: 30,
+    label: "30 days",
+  },
+  {
+    value: 60,
+    label: "60 days",
+  },
+  {
+    value: 90,
+    label: "90 days",
+  },
+  {
+    value: 120,
+    label: "120 days",
+  },
+];
+
+const smsLimit = [
+  {
+    value: 450,
+    label: "450",
+  },
+  {
+    value: 900,
+    label: "Unlimited",
+  },
+];
+
+const socialMediaLimit = [
+  {
+    value: 20,
+    label: "20 GB",
+  },
+  {
+    value: 40,
+    label: "Unlimited",
+  },
+];
+const dataLimit = [
+  {
+    value: 0,
+    label: "0GB",
+  },
+  {
+    value: 50,
+    label: "50GB",
+  },
+  {
+    value: 100,
+    label: "100GB",
+  },
+  {
+    value: 150,
+    label: "150GB",
+  },
+  {
+    value: 200,
+    label: "200GB",
+  },
+  {
+    value: 250,
+    label: "250GB",
+  },
+  {
+    value: 300,
+    label: "Unlimited",
+  },
+];
+export { validity, smsLimit, dataLimit, socialMediaLimit };
